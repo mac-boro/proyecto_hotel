ec574de6e0b15c15d003db11209c886a0f1b2b57
mac-boro <mad.mac.boro@gmail.com>
2025-11-10 00:31:35 -0300

    modified:   datos/obtener_datos.py

--- datos/obtener_datos.py
+++ datos/obtener_datos.py
@@ -19,11 +19,11 @@ def obtener_objeto_deshab(objeto):
         print(f"Error: {e}")
 
 def obtener_huesped_nombre(nombre_huesped):
-    listado_huesped = obtener_objeto(Huesped)
+    listado_huesped = obtener_objeto_hab(Huesped)
     huesped_encontrado = None
     if listado_huesped:
         for nombre in listado_huesped:
-            if normalizar_cadena(nombre) == normalizar_cadena(nombre_huesped):
+            if normalizar_cadena(nombre.nombre_huesped) == normalizar_cadena(nombre_huesped):
                 huesped_encontrado = nombre
                 break
     return huesped_encontrado
