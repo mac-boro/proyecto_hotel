af2711ae2ebdee6a553311cb4013afbc6d271b54
mac-boro <mad.mac.boro@gmail.com>
2025-11-12 17:15:57 -0300

    modified:   datos/obtener_datos.py 	new file:   negocio/habitacion_update.py 	modified:   negocio/reserva_create.py

--- negocio/reserva_create.py
+++ negocio/reserva_create.py
@@ -1,6 +1,7 @@
 from modelos.reserva import Reserva
 from datos.insertar_datos import insertar_objeto
 from negocio.habitacion_read import lista_habitaciones
+from negocio.habitacion_update import modificar_habitacion_hab
 from negocio.huesped_create import insertar_huesped_reserva
 from ui.iu_habitacion import ingresar_habitacion_reserva
 from ui.iu_servicio import solicitar_servicio
@@ -19,4 +20,6 @@ def crear_reserva():
 
     nueva_reserva = Reserva(id_huesped=huesped, id_habitacion=habitacion, id_descuento=descuento, id_servicio=servicio, fecha_entrada=fecha_entrada, fecha_salida=fecha_salida, total=total)
     insertar_objeto(nueva_reserva)
+    modificar_habitacion_hab(habitacion)
+
     print('¡Reserva creada exitosamente!')
